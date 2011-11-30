3c80f298abdf3e1c230454fdfd74e65afb422e85
cesaraugusto <cesaraugusto@liberiun.com>
2011-11-30 10:50:54 -0200

    Criação view Organizational Struture

--- vindula/content/content/orgstructure.py
+++ vindula/content/content/orgstructure.py
@@ -7,7 +7,9 @@ from plone.app.textfield import RichText
 from plone.formwidget.contenttree import ObjPathSourceBinder
 from vindula.content import MessageFactory as _
 from vindula.controlpanel.vocabularies import ControlPanelObjects
+from plone.uuid.interfaces import IUUID
 
+from vindula.myvindula.user import BaseFunc, ModelsFuncDetails, ModelsMyvindulaHowareu
 
 # Interface and schema
 
@@ -58,3 +60,42 @@ class IOrganizationalStructure(form.Schema):
             ),
         required=False,
         )
+    
+    
+    
+class OrganizationalStructure(grok.View):
+    grok.context(IOrganizationalStructure)
+    grok.require('zope2.View')
+    grok.name('view')
+    
+    def get_UID(self):
+        return IUUID(self.context)
+    
+    def get_howareu_departament(self, departament):
+        D={}
+        D['visible_area'] = departament
+        return ModelsMyvindulaHowareu().get_myvindula_howareu(**D)
+    
+    def get_prefs_user(self, user):
+        try:
+            user_id = unicode(user, 'utf-8')    
+        except:
+            user_id = user 
+
+        return ModelsFuncDetails().get_FuncDetails(user_id)
+
+    def getPhoto(self,photo):
+        prefs_user = self.get_prefs_user(photo)
+        if prefs_user:
+            if prefs_user.photograph is not None and \
+                not ' ' in prefs_user.photograph  and \
+                not prefs_user.photograph == '':
+                return BaseFunc().get_imageVindulaUser(prefs_user.photograph)
+                #return self.context.absolute_url()+'/'+prefs_user.photograph # + '/image_thumb'
+            else:
+                return self.context.absolute_url()+'/defaultUser.png'
+        else:
+            return self.context.absolute_url()+'/defaultUser.png'
+    
+
+
