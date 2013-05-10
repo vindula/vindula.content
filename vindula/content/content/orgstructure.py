28968b43e0d1d1599a32505e4bb1720a89b17ca1
Cesar Augusto <cesaraugusto@liberiun.com>
2013-05-10 16:11:49 -0300

    Ajuste sigla estrutura organizacional

--- vindula/content/content/orgstructure.py
+++ vindula/content/content/orgstructure.py
@@ -10,7 +10,7 @@ from zope.interface import Interface
 from AccessControl import ClassSecurityInfo
 from Products.CMFCore.utils import getToolByName
 from zope.interface import Interface
-from zope.app.container.interfaces import IObjectAddedEvent 
+from zope.app.container.interfaces import IObjectAddedEvent
 from Products.CMFPlone.utils import _createObjectByType
 from zExceptions import BadRequest
 
@@ -37,16 +37,16 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
 
     TextField(
             name='siglaunidade',
-            default_content_type = 'text/restructured',
-            default_output_type = 'text/x-html-safe',
+            # default_content_type = 'text/restructured',
+            # default_output_type = 'text/x-html-safe',
             widget=StringWidget(
                 label=_(u"Sigla da Unidade"),
                 description=_(u"Informe a sigla da Unidade."),
-                rows="10",
+                # rows="10",
             ),
             required=True,
     ),
-    
+
     ReferenceField('structures',
         multiValued=0,
         allowed_types=('OrganizationalStructure',),
@@ -138,7 +138,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             required=0,
             schemata = 'Permissões',
             ),
-    
+
      LinesField(
             name="Groups_edit",
             multiValued=1,
@@ -150,7 +150,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             required=0,
             schemata = 'Permissões',
             ),
-    
+
      LinesField(
             name="Groups_admin",
             multiValued=1,
@@ -163,7 +163,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             schemata = 'Permissões',
             validators = ('isUserUpdate',),
             ),
-    
+
     #---------------------abas de permissões no Objeto---------------------------------
 
     BooleanField(
@@ -174,7 +174,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description='Se selecionado, Ativa a opção de personalização dos itens inferiores a Unidade Organizacional.',
         ),
         schemata = 'Layout'
-    ),                                                       
+    ),
 
     StringField(
         name='corGeralPortal',
@@ -185,8 +185,8 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description="Cor para esta área, todo o layout desta área usará esta cor.",
         ),
         schemata = 'Layout'
-    ),    
-                                                                   
+    ),
+
     ReferenceField('logoPortal',
         multiValued=0,
         allowed_types=('Image'),
@@ -197,7 +197,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description='Será exibido no topo do portal desta área. A imagem será redimensionada para um tamanho adequado.'),
         schemata = 'Layout'
     ),
-                                                                   
+
     ReferenceField('logoRodape',
         multiValued=0,
         allowed_types=('Image'),
@@ -208,7 +208,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             label=_(u"Logo Rodapé"),
             description='Será exibido no rodapé desta área o imagem selecionada. A imagem será redimensionada para um tamanho adequado.'),
         schemata = 'Layout'
-    ),                                                                   
+    ),
 
 
     #-----------BackGroup do portal------------------
@@ -221,8 +221,8 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             label=_(u"Imagem de fundo do portal"),
             description='A imagem será aplicada no background do portal. A imagem será mostrada em seu tamanho original, sem repetição.'),
         schemata = 'Layout'
-    ),                                                                   
-    
+    ),
+
     StringField(
         name = 'posicaoImageBackground',
         widget=SelectionWidget(
@@ -233,8 +233,8 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
         vocabulary = [('no-repeat', 'Centralizar'), ('repeat', 'Repetir na pagina toda'), ('repeat-x', 'Repetir horizontalmente'), ('repeat-y', 'Repetir verticamente'),],
         default='no-repeat',
         schemata = 'Layout'
-    ),        
-    
+    ),
+
     StringField(
         name='corBackground',
         searchable=0,
@@ -245,7 +245,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
         ),
         schemata = 'Layout'
     ),
-    
+
     ReferenceField('imageFooter',
         multiValued=0,
         allowed_types=('Image'),
@@ -258,7 +258,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
     ),
 
     #-----------Menu do portal------------------#
-    
+
     StringField(
         name='corMenuFundo',
         searchable=0,
@@ -269,7 +269,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
         ),
         schemata = 'Menu'
     ),
-                                                            
+
     StringField(
         name='corMenuFonte',
         searchable=0,
@@ -280,7 +280,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
         ),
         schemata = 'Menu'
     ),
-            
+
     StringField(
         name='corMenuHoverDropdown',
         searchable=0,
@@ -290,7 +290,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description="Clique <a class='visualizacao' href='/++resource++vindula.controlpanel/menu/corMenuHoverDropdown.png'>aqui para exemplo</a>",
         ),
         schemata = 'Menu'
-    ),   
+    ),
 
     ReferenceField('imageSubmenuBkg',
         multiValued=0,
@@ -303,7 +303,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
                          A imagem será mostrada com a sua largura original, com repetição.'),
         schemata = 'Menu'
     ),
-                                                        
+
     ReferenceField('imageMenuBkg',
         multiValued=0,
         allowed_types=('Image'),
@@ -315,7 +315,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
                          A imagem será mostrada em seu tamanho original, com repetição.'),
         schemata = 'Menu'
     ),
-    
+
     StringField(
         name='corMenuFonteDropdown',
         searchable=0,
@@ -325,8 +325,8 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description="Clique <a class='visualizacao' href='/++resource++vindula.controlpanel/menu/corMenuFonteDropdown.png'>aqui para exemplo</a>",
         ),
         schemata = 'Menu'
-    ), 
-    
+    ),
+
     StringField(
         name='corMenuFonteHoverDropdown',
         searchable=0,
@@ -337,7 +337,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
         ),
         schemata = 'Menu'
     ),
-    
+
     StringField(
         name='corMenuDropdownHover',
         searchable=0,
@@ -347,8 +347,8 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description="Clique <a class='visualizacao' href='/++resource++vindula.controlpanel/menu/corMenuDropdownHover.png'>aqui para exemplo</a>",
         ),
         schemata = 'Menu'
-    ),     
-    
+    ),
+
     StringField(
         name='corMenuSelected',
         searchable=0,
@@ -358,8 +358,8 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description="Clique <a class='visualizacao' href='/++resource++vindula.controlpanel/menu/corMenuSelected.png'>aqui para exemplo</a>",
         ),
         schemata = 'Menu'
-    ),   
-    
+    ),
+
     StringField(
         name='corMenuFonteSelected',
         searchable=0,
@@ -369,7 +369,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description="Clique <a class='visualizacao' href='/++resource++vindula.controlpanel/menu/corMenuFonteSelected.png'>aqui para exemplo</a>",
         ),
         schemata = 'Menu'
-    ),   
+    ),
 
     StringField(
         name='corMenuSelectedDropdown',
@@ -380,7 +380,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
             description="Clique <a class='visualizacao' href='/++resource++vindula.controlpanel/menu/corMenuSelectedDropdown.png'>aqui para exemplo</a>",
         ),
         schemata = 'Menu'
-    ),                                                  
+    ),
 
     StringField(
         name='corMenuFonteSelectedDropdown',
@@ -394,7 +394,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
     ),
 
     # CONFIGURACAO DO TEMA DO PORTLET
-    
+
     ReferenceField('imageTopPortlet',
         multiValued=0,
         allowed_types=('Image'),
@@ -406,7 +406,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
         schemata = 'Portlet'
 
     ),
-    
+
     IntegerField(
         name='heightTopPortlet',
         widget=IntegerWidget(
@@ -415,7 +415,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
         ),
         schemata = 'Portlet'
     ),
-    
+
     ReferenceField('imageMiddlePortlet',
         multiValued=0,
         allowed_types=('Image'),
@@ -437,7 +437,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
                          A imagem será mostrada com a sua altura original, com repetição.'),
         schemata = 'Portlet'
     ),
-    
+
     IntegerField(
         name='heightBottomPortlet',
         widget=IntegerWidget(
@@ -453,7 +453,7 @@ OrganizationalStructure_schema =  ATFolder.schema.copy() + Schema((
 finalizeATCTSchema(OrganizationalStructure_schema, folderish=True)
 invisivel = {'view':'invisible','edit':'invisible',}
 # Dates
-L = ['effectiveDate','expirationDate','creation_date','modification_date']   
+L = ['effectiveDate','expirationDate','creation_date','modification_date']
 # Categorization
 L += ['subject','relatedItems','location','language']
 # Ownership
@@ -462,8 +462,8 @@ L += ['creators','contributors','rights']
 L += ['allowDiscussion','excludeFromNav', 'nextPreviousEnabled']
 
 for i in L:
-    OrganizationalStructure_schema[i].widget.visible = invisivel 
-    
+    OrganizationalStructure_schema[i].widget.visible = invisivel
+
 class OrganizationalStructure(ATFolder):
     """ OrganizationalStructure """
 
@@ -472,7 +472,7 @@ class OrganizationalStructure(ATFolder):
     portal_type = 'OrganizationalStructure'
     _at_rename_after_creation = True
     schema = OrganizationalStructure_schema
-    
+
     def at_post_create_script(self):
         """Notify that the employee has been saved.
         """
@@ -482,17 +482,17 @@ class OrganizationalStructure(ATFolder):
         """Notify that the employee has been saved.
         """
         notify(OrgstructureModifiedEvent(self))
-    
+
     def voc_listGroups(self):
         terms = []
         if 'acl_users' in getSite().keys():
-            groups = getSite().get('acl_users').getGroups() 
-            
+            groups = getSite().get('acl_users').getGroups()
+
             for group in groups:
                 member_id = group.id
                 member_name = group.getProperties().get('title','')
                 terms.append((member_id, unicode(member_name)))
-        
+
         return terms
 
     def voc_itens_menu(self):
@@ -510,7 +510,7 @@ class OrganizationalStructure(ATFolder):
                result.append((modalidade, modalidade))
         return result
 
-registerType(OrganizationalStructure, PROJECTNAME) 
+registerType(OrganizationalStructure, PROJECTNAME)
 
 class OrgstructureModifiedEvent(object):
     """Event to notify that Orgstructure have been saved.
@@ -528,58 +528,58 @@ class OrgstructureModifiedEvent(object):
 def CreatGroupInPloneSite(event):
     ctx = event.context
     ctxPai = ctx.aq_parent
-    portalGroup = getSite().portal_groups 
+    portalGroup = getSite().portal_groups
     tipos = [{'tipo':'view' ,'name':'',              'permissao':['Reader']                                    },
              {'tipo':'edit' ,'name':' - Edição',      'permissao':['Reviewer','Reader','Contributor']           },
              {'tipo':'admin','name':' - Administração', 'permissao':['Editor','Reviewer','Reader','Contributor']}
             ]
-    
+
     for tipo in tipos:
-        id_grupo = ctx.UID() +'-'+tipo['tipo']    
-    
+        id_grupo = ctx.UID() +'-'+tipo['tipo']
+
         if not id_grupo in portalGroup.listGroupIds():
             if ctxPai.portal_type == 'OrganizationalStructure':
                 paiTitle = ctxPai.title
             else:
                 paiTitle = ''
-            
+
             nome_grupo = 'EO: '+ paiTitle +"\\"  + ctx.title + tipo['name']
             portalGroup.addGroup(id_grupo, title=nome_grupo)
             #Adiciona o grupo a 'AuthenticatedUsers'
             portalGroup.getGroupById('AuthenticatedUsers').addMember(id_grupo)
-             
-            ctx.manage_setLocalRoles(id_grupo, tipo['permissao'])  
-        
+
+            ctx.manage_setLocalRoles(id_grupo, tipo['permissao'])
+
         for view in eval('ctx.getGroups_'+tipo['tipo']+'()'):
             portalGroup.getGroupById(id_grupo).addMember(view)
-    
+
         if ctxPai.portal_type == 'OrganizationalStructure':
             group_pai = ctxPai.UID()+"-view"
             portalGroup.getGroupById(group_pai).addMember(id_grupo)
 
-    
+
     id_grupo_employees = ctx.UID() +'-view'
     new_tupla = list(ctx.Groups_view)
     for user in ctx.getEmployees():
         if new_tupla.count(user) == 0:
             new_tupla.append(user)
-        
+
         portalGroup.getGroupById(id_grupo_employees).addMember(user)
     ctx.Groups_view = tuple(new_tupla)
 
-@grok.subscribe(IOrganizationalStructure, IObjectRemovedEvent)        
+@grok.subscribe(IOrganizationalStructure, IObjectRemovedEvent)
 def RemoveGroupInPloneSite(context, event):
-    portalGroup = getSite().portal_groups 
+    portalGroup = getSite().portal_groups
     tipos = [{'tipo':'view' ,'name':'',              'permissao':['Reader']                                    },
              {'tipo':'edit' ,'name':' - Edição',      'permissao':['Reviewer','Reader','Contributor']           },
              {'tipo':'admin','name':' - Administração', 'permissao':['Editor','Reviewer','Reader','Contributor']}
             ]
-    
+
     for tipo in tipos:
         id_grupo = context.UID() +'-'+tipo['tipo']
         try: portalGroup.removeGroup(id_grupo)
         except: pass
-        
+
 CONTEUDOS = [{'id':'discussoes', 'titulo':u'Discussões', 'tipo':'Ploneboard'},
              {'id':'paginas', 'titulo':u'Páginas', 'tipo':'VindulaFolder'},
              {'id':'arquivos', 'titulo':u'Arquivos', 'tipo':'VindulaFolder'},
@@ -593,8 +593,8 @@ CONTEUDOS = [{'id':'discussoes', 'titulo':u'Discussões', 'tipo':'Ploneboard'},
              {'id':'classificado', 'titulo':u'Classificados', 'tipo':'Classifieds'},
              {'id':'reserva', 'titulo':u'Reserva', 'tipo':'ContentReserve'},
              {'id':'formulario', 'titulo':u'Formulário', 'tipo':'VindulaFolder'}
-             ]      
-        
+             ]
+
 def _cria_objeto(objeto, conteudos):
     """ Metodo para a criacao de conteudo
    """
@@ -604,7 +604,7 @@ def _cria_objeto(objeto, conteudos):
         id = conteudo['id']
         tipo = conteudo['tipo']
         titulo = conteudo['titulo']
-        
+
         try:
             _createObjectByType(tipo, objeto, id=id, title=titulo)
         except BadRequest:
@@ -612,41 +612,41 @@ def _cria_objeto(objeto, conteudos):
 
 @grok.subscribe(IOrganizationalStructure, IObjectAddedEvent)
 def CreatElemetsOrganizationalStructure(context, event):
-    
-    
+
+
 #    import pdb; pdb.set_trace()
-    
-   
-    
+
+
+
     _cria_objeto(context, CONTEUDOS)
 
 
-        
+
 class OrganizationalStructureView(grok.View, UtilMyvindula):
     grok.context(IOrganizationalStructure)
     grok.require('zope2.View')
     grok.name('view_organizational')
-    
+
     def get_UID(self):
         return IUUID(self.context)
-    
+
     def get_howareu_departament(self, departament):
         D={}
         D['visible_area'] = departament
         return ModelsMyvindulaHowareu().get_myvindula_howareu(**D)
-    
+
     def get_department(self, user):
         try:
-            user_id = unicode(user, 'utf-8')    
+            user_id = unicode(user, 'utf-8')
         except:
             user_id = user
-        
-        return ModelsDepartment().get_departmentByUsername(user_id)    
+
+        return ModelsDepartment().get_departmentByUsername(user_id)
 
     def get_LastContent(self):
         ctool = getSite().portal_catalog
         objs = ctool(path = {'query': '/'.join(self.context.getPhysicalPath()), 'depth': 1},
-                      sort_on='modified', sort_order='decrescent')   
+                      sort_on='modified', sort_order='decrescent')
 
         if objs:
             return objs
@@ -661,14 +661,14 @@ class FolderOrganizationalStructureView(grok.View, BaseFunc):
 
     def getCategorias(self):
         return OrganizationalStructure(self.context).voc_categoria();
-    
+
     def getOrgStruc(self):
         catalog = getToolByName(self.context, 'portal_catalog')
         L = []
-        
+
         if 'categoria' in self.request.form.keys():
             categoria = self.request.form.get('categoria')
-        
+
         if categoria:
             results = catalog(portal_type='OrganizationalStructure',
                       review_state='published',
