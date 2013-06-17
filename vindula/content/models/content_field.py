068b0f7411b5b9df3be1e02418459d948e57b9c6
Cesar Augusto <cesaraugusto@liberiun.com>
2013-06-17 10:14:58 -0300

    Ajustes na busca de contentFields para objetos excluidos

--- vindula/content/models/content_field.py
+++ vindula/content/models/content_field.py
@@ -6,27 +6,23 @@ from storm.locals import *
 from storm.expr import Desc, Select
 
 
-from vindula.myvindula.models.base import BaseStore
+from vindula.myvindula.models.base import BaseStoreMyvindula
 
-    
 
-class ContentField(Storm, BaseStore):
+
+class ContentField(Storm, BaseStoreMyvindula):
     __storm_table__ = 'vinapp_content_contentfield'
-    
+
     id = Int(primary=True)
     name = Unicode()
-    
-    date_created = DateTime()
-    date_modified = DateTime()
-    date_excluded = DateTime()
-    
     values = ReferenceSet(id, "ContentFieldData.field_id")
-        
+
     def get_content_file_by_type(self,type):
         L = []
-        data = self.store.find(ContentField, ContentField.name==type).one()
+        data = self.store.find(ContentField, ContentField.name==type, ContentField.deleted==False).one()
         if data:
             for item in data.values:
-                L.append(item.value)
-            
+                if not item.deleted:
+                    L.append(item.value)
+
         return L
