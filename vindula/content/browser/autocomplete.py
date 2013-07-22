afb27707e1b330d10ff647f203a5bdd4e807dcd7
Denis Raison <denisraison@liberiun.com>
2013-07-22 16:13:57 -0300

    Bug fix

--- vindula/content/browser/autocomplete.py
+++ vindula/content/browser/autocomplete.py
@@ -168,7 +168,7 @@ class AutocompleteView(grok.View):
                 try:
                     value = eval(value.value)
                     value = value[0]
-                except NameError:
+                except(SyntaxError, NameError):
                     value = value.value
                 value = uuidToObject(value)
             else:
