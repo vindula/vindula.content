88ae68f8b7ff46fd1ea9aa45f8a93362521de99d
Denis Raison <denisraison@liberiun.com>
2013-09-17 17:32:16 -0300

    Adicionado sistema de accordion do vindula e implementado na biblioteca

--- vindula/content/static/js/vindula_accordion.js
+++ vindula/content/static/js/vindula_accordion.js
@@ -8,4 +8,15 @@ $j(document).ready(function(){
         $accordion.toggleClass('active');
     });
 
+    $('.accordion').click(function(ev){
+        $hide_ele = $j('[accordion-id="'+this.id+ '"]');
+        $hide_ele.toggle(300);
+        
+        return false;
+    })
+    
+    $j('[accordion-collapse=true]')
+      .each(function(){
+          $j(this).hide();
+      }); 
 });
