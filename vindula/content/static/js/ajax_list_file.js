80ace040ecf3a1bd6802cad06dfe4365ee543b07
Denis Raison <denisraison@liberiun.com>
2014-04-01 09:51:30 -0300

    Ajustes na sintaxe do js de paginação

--- vindula/content/static/js/ajax_list_file.js
+++ vindula/content/static/js/ajax_list_file.js
@@ -95,11 +95,11 @@ function executaAjax(ctx, b_start, b_size, sort_on, params){
                 dom.find('.social-box').vindula(null, {user_token: window.token});    
             }
             
-            $('a.button-action', dom).click(function(event){
+            $j('a.button-action', dom).click(function(event){
                 event.preventDefault();
                 event.stopPropagation();
                 var url = '/vindula-api/social/combo/action_tabular/';
-                url = url.concat(window.token+'/'+$(this).attr('type_object')+'/'+this.id);
+                url = url.concat(window.token+'/'+$j(this).attr('type_object')+'/'+this.id);
                 
                 $j(window.parent.document).vindula('add_combo_action_tabular', {'id': this.id, 'src': url, 'iframe_class': 'new_combo_action_tabular', 'left': '-17px'});
             });
