80c49237fffbd413bd56300a370bd66a9779fc00
Denis Raison <denisraison@liberiun.com>
2014-05-30 11:10:51 -0300

    Ajustes no script de download

--- vindula/content/skins/vindula_content_templates/at_download.py
+++ vindula/content/skins/vindula_content_templates/at_download.py
@@ -7,12 +7,19 @@
 ##parameters=
 ##title=Download a file keeping the original uploaded filename
 ##
-if context.activ_download:
 
+if getattr(context, 'activ_download', False):
+    can_download = getattr(context, 'activ_download')
+else:
+    can_download = True
+
+if can_download:
     if traverse_subpath:
         field = context.getWrappedField(traverse_subpath[0])
     else:
         field = context.getPrimaryField()
+
+
     if not hasattr(field, 'download'):
         from zExceptions import NotFound
         raise NotFound
