48768fec34750a24efec3393c55b5a1088d63f18
Cesar Augusto <cesaraugusto@liberiun.com>
2014-10-24 10:03:10 -0200

    Ajustes

--- vindula/content/content/vindulafolder.py
+++ vindula/content/content/vindulafolder.py
@@ -16,15 +16,15 @@ from zope.interface import implements
 from vindula.content.config import *
 from Products.Archetypes.atapi import *
 from Products.ATContentTypes.content.schemata import finalizeATCTSchema
-   
 
-from zope.schema.interfaces import IVocabularyFactory   
+
+from zope.schema.interfaces import IVocabularyFactory
 from zope.component import queryUtility
 
 from hashlib import md5
 
 VindulaFolder_schema =  ATFolder.schema.copy() + Schema((
-    
+
     StringField(
         name='itens_menu',
         widget=InAndOutWidget(
@@ -44,43 +44,43 @@ invisivel = {'view':'invisible','edit':'invisible',}
 
 class VindulaFolder(ATFolder):
     """ VindulaFolder """
-    
+
     security = ClassSecurityInfo()
     implements(IVindulaFolder, IRuleAssignable)
     portal_type = 'VindulaFolder'
     _at_rename_after_creation = True
     schema = VindulaFolder_schema
-    
+
     def voc_itens_menu(self):
         types = self.portal_types.listContentTypes()
         return types
- 
 
-registerType(VindulaFolder, PROJECTNAME) 
 
-    
+registerType(VindulaFolder, PROJECTNAME)
+
+
 #class VindulaFolderView(grok.View):
 #    grok.context(IVindulaFolder)
 #    grok.require('zope2.View')
 #    grok.name('view')
-#    
+#
 
-class VindulaFolderSync(grok.View):
-    grok.context(IVindulaFolder)
-    grok.require('zope2.View')
-    grok.name('enable-sync')
+# class VindulaFolderSync(grok.View):
+#     grok.context(IVindulaFolder)
+#     grok.require('zope2.View')
+#     grok.name('enable-sync')
 
-    def __init__(self,context,request):
-        super(VindulaFolderSync,self).__init__(context,request)
-        context = self.context
-        self.portal_type = context.portal_type
-        self.UID = context.UID()
+#     def __init__(self,context,request):
+#         super(VindulaFolderSync,self).__init__(context,request)
+#         context = self.context
+#         self.portal_type = context.portal_type
+#         self.UID = context.UID()
 
-    def get_id_frame(self):
-        return md5(self.portal_type + self.UID).hexdigest()
+#     def get_id_frame(self):
+#         return md5(self.portal_type + self.UID).hexdigest()
 
-    def get_url_frame(self):
-        url = self.context.portal_url()
-        user_token = self.request.SESSION.get('user_token')
+#     def get_url_frame(self):
+#         url = self.context.portal_url()
+#         user_token = self.request.SESSION.get('user_token')
 
-        return '%s/vindula-api/sync/habilitar/%s/%s/%s?iframe_id=%s' %(url,user_token,self.portal_type,self.UID,self.get_id_frame())
+#         return '%s/vindula-api/sync/habilitar/%s/%s/%s?iframe_id=%s' %(url,user_token,self.portal_type,self.UID,self.get_id_frame())
